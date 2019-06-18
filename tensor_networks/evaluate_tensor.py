66e112b8434cd4a2b857e847278884a99480ef3e
Michael A. Perlin <mika.perlin@gmail.com>
2019-06-18 10:56:13 -0500

    added bubbling algorithm for computing (non-unitary) tensor network still requires benchmarking on random tensor networks

--- tensor_networks/evaluate_tensor.py
+++ tensor_networks/evaluate_tensor.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+np.set_printoptions(linewidth = 200)
 
 import tensorflow as tf
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
@@ -59,6 +60,110 @@ def make_net():
     return net, nodes, edges
 
 net, nodes, edges = make_net()
-
+##########################################################################################
+# for node_key, node in nodes.items():
+    # if node_key == (0,0): new_node = nodes[0,0]
+    # else: new_node = net.contract_between(new_node, node)
+    # state = new_node.get_tensor().numpy().flatten()
+    # print(state)
+    # print(node_key, "vals, sum:",
+          # sorted(set(state[:].real.flatten())),
+          # np.sum(state[:].real.astype(int)))
 tn.contractors.naive(net)
+##########################################################################################
 print(net.get_final_node().tensor.numpy())
+print()
+
+
+print("-"*80 + "\n")
+##########################################################################################
+##########################################################################################
+##########################################################################################
+
+import qutip as qt
+
+net, nodes, edges = make_net()
+
+edge_idx = { edge : idx for idx, edge in enumerate(list(edges.values())) }
+
+eaten_nodes = set()
+dangling_edges = []
+
+state = qt.basis(1,0)
+for node_key, node in nodes.items():
+    # identify input/output edges to this node, and the corresponding axes
+    inp_edges, inp_axes = [], []
+    out_edges, out_axes = [], []
+    for edge in node.get_all_edges():
+        if node == edge.node1:
+            axis_to_other = edge.axis1
+            other_node = edge.node2
+        else:
+            axis_to_other = edge.axis2
+            other_node = edge.node1
+        if other_node in eaten_nodes:
+            inp_edges.append(edge)
+            inp_axes.append(axis_to_other)
+        else:
+            out_edges.append(edge)
+            out_axes.append(axis_to_other)
+
+    # identify auxiliary dangling edges that do not participate in swallowing this node
+    aux_edges = [ edge for edge in dangling_edges if edge not in inp_edges ]
+
+    # identify number of input, output, and auxiliary edges
+    inp_num = len(inp_edges)
+    out_num = len(out_edges)
+    aux_num = len(aux_edges)
+
+    # total number of qubits necessary to swallow this node
+    total_num = max(inp_num,out_num) + aux_num
+
+    # identify the index (location) of input, auxiliary, and ancilla qubits (<--> edges)
+    node_inp_idx = [ dangling_edges.index(edge) for edge in inp_edges ]
+    node_aux_idx = [ dangling_edges.index(edge) for edge in aux_edges ]
+    node_anc_idx = list(range(len(dangling_edges), total_num))
+
+    # attach ancillas to the current state, if necessary
+    if out_num > inp_num:
+        ancillas = [ qt.basis(2,0) ] * ( out_num - inp_num )
+        if state.dims == [[1]]*2:
+            state = state[0,0] * qt.tensor(*ancillas)
+        else:
+            state = qt.tensor(state, *ancillas)
+
+    # get the tensor associated with this node, reordering axes appropriately
+    tensor = tf.transpose(node.get_tensor(), out_axes + inp_axes)
+
+    # convert the tensor into a 2-D matrix addressing input/output degrees of freedom
+    matrix_shape = (2**out_num, 2**inp_num)
+    matrix = tf.reshape(tensor, matrix_shape)
+
+    # make the matrix square by padding it in with zeros as necessary
+    np_matrix = matrix.numpy()
+    matrix_padding = [ (0, 2**max(inp_num,out_num) - matrix_shape[jj])
+                       for jj in range(2) ]
+    np_matrix = np.pad(np_matrix, matrix_padding, mode = "constant", constant_values = 0)
+    np_matrix = np.kron(np_matrix, np.eye(2**aux_num))
+    qt_matrix = qt.Qobj(np_matrix, dims = [ [2]*total_num ]*2) # convert to qutip object
+
+    # rearrange qubit order to match that of the swallowing operator
+    op_inp_bit_idx = node_anc_idx + node_inp_idx + node_aux_idx
+    state = state.permute(op_inp_bit_idx)
+
+    # act on the current state by the swallowing operator
+    state = qt_matrix * state
+
+    # remove extra qubits if necessary
+    trim_num = max(0, inp_num - out_num)
+    if trim_num > 0:
+        total_num -= trim_num
+        state = qt.Qobj(state[:2**total_num], dims = [ [2]*total_num, [1]*total_num ])
+
+    # add to our list of "eaten" nodes, update the list of dangling edges
+    eaten_nodes.add(node)
+    for edge in inp_edges:
+        dangling_edges.remove(edge)
+    dangling_edges = out_edges + dangling_edges
+
+print(state[:].real[0,0])
