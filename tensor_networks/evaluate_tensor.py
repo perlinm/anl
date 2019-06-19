6258131ab3bd2d42e95d42659c02302a6c1ecf58
Michael A. Perlin <mika.perlin@gmail.com>
2019-06-18 22:50:22 -0500

    switch from using qutip to using tensorflow in bubbling algorithm note: bubbling algorithm currently does not use ancillas properly

--- tensor_networks/evaluate_tensor.py
+++ tensor_networks/evaluate_tensor.py
@@ -12,6 +12,7 @@ import tensornetwork as tn
 
 temp = 1 # temperature
 lattice_shape = (3,3) # lattice sites per axis
+print_node = (0,1)
 
 # value of tensor for particular indices
 def tensor_val(idx):
@@ -61,16 +62,18 @@ print(net.get_final_node().tensor.numpy())
 # evaluation of tensor network via bubbling
 ##########################################################################################
 
-import qutip as qt
+def zero_state(length):
+    length = max(0,length)
+    return tf.reshape(tf.one_hot(0, 2**length, dtype = tf.float64), (2,)*length)
 
 net, nodes, edges = make_net()
-bubbling_order = nodes.values()
+# bubbling_order = nodes.values()
 
 eaten_nodes = set()
 dangling_edges = []
 
-state = qt.basis(1,0)
-for node in bubbling_order:
+state = zero_state(0)
+for node_key, node in nodes.items():
     # identify input/output edges to this node, and the corresponding axes
     inp_edges, inp_axes = [], []
     out_edges, out_axes = [], []
@@ -88,59 +91,18 @@ for node in bubbling_order:
             out_edges.append(edge)
             out_axes.append(axis_to_other)
 
-    # identify auxiliary dangling edges that do not participate in swallowing this node
-    aux_edges = [ edge for edge in dangling_edges if edge not in inp_edges ]
-
-    # identify number of input, output, and auxiliary edges
-    inp_num = len(inp_edges)
-    out_num = len(out_edges)
-    aux_num = len(aux_edges)
-
-    # total number of qubits necessary to swallow this node
-    total_num = max(inp_num,out_num) + aux_num
-
-    # identify the index (location) of input, auxiliary, and ancilla qubits (<--> edges)
-    node_inp_idx = [ dangling_edges.index(edge) for edge in inp_edges ]
-    node_aux_idx = [ dangling_edges.index(edge) for edge in aux_edges ]
-    node_anc_idx = list(range(len(dangling_edges), total_num))
-
-    # attach ancillas to the current state, if necessary
-    if out_num > inp_num:
-        ancillas = [ qt.basis(2,0) ] * ( out_num - inp_num )
-        if state.dims == [[1]]*2:
-            state = state[0,0] * qt.tensor(*ancillas)
-        else:
-            state = qt.tensor(state, *ancillas)
-
     # get the tensor associated with this node, reordering axes appropriately
-    tensor = tf.transpose(node.get_tensor(), out_axes + inp_axes)
+    tensor = tf.transpose(node.get_tensor(), inp_axes + out_axes)
 
-    # convert the tensor into a 2-D matrix addressing input/output degrees of freedom
-    matrix_shape = (2**out_num, 2**inp_num)
-    matrix = tf.reshape(tensor, matrix_shape)
-
-    # make the matrix square by padding it in with zeros as necessary
-    np_matrix = matrix.numpy()
-    matrix_padding = [ (0, 2**max(inp_num,out_num) - matrix_shape[jj]) for jj in range(2) ]
-    np_matrix = np.pad(np_matrix, matrix_padding, mode = "constant", constant_values = 0)
-    np_matrix = np.kron(np_matrix, np.eye(2**aux_num))
-    qt_matrix = qt.Qobj(np_matrix, dims = [ [2]*total_num ]*2) # convert to qutip object
-
-    # rearrange qubit order to match that of the swallowing operator
-    state = state.permute(node_anc_idx + node_inp_idx + node_aux_idx)
-
-    # act on the current state by the swallowing operator
-    state = qt_matrix * state
-
-    # remove extra qubits if necessary
-    if out_num < inp_num:
-        kept_num = total_num - inp_num + out_num
-        state = qt.Qobj(state[:2**kept_num], dims = [ [2]*kept_num, [1]*kept_num ])
+    # act the swallowing operator on the state
+    state_axes = [ dangling_edges.index(edge) for edge in inp_edges ]
+    tensor_axes = list(range(len(inp_edges)))
+    state = tf.tensordot(state, tensor, axes = [ state_axes, tensor_axes ])
 
     # add to our list of "eaten" nodes, update the list of dangling edges
     eaten_nodes.add(node)
     for edge in inp_edges:
         dangling_edges.remove(edge)
-    dangling_edges = out_edges + dangling_edges
+    dangling_edges += out_edges
 
-print(state[:].real[0,0])
+print(state.numpy())
