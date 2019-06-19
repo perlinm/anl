13b5b13fcc69598842350aa8795fdd38b0358179
Michael A. Perlin <mika.perlin@gmail.com>
2019-06-19 10:42:31 -0500

    add ancillas properly in bubbling algorithm

--- tensor_networks/evaluate_tensor.py
+++ tensor_networks/evaluate_tensor.py
@@ -12,7 +12,6 @@ import tensornetwork as tn
 
 temp = 1 # temperature
 lattice_shape = (3,3) # lattice sites per axis
-print_node = (0,1)
 
 # value of tensor for particular indices
 def tensor_val(idx):
@@ -67,13 +66,14 @@ def zero_state(length):
     return tf.reshape(tf.one_hot(0, 2**length, dtype = tf.float64), (2,)*length)
 
 net, nodes, edges = make_net()
-# bubbling_order = nodes.values()
+bubbling_order = nodes.values()
 
 eaten_nodes = set()
 dangling_edges = []
 
 state = zero_state(0)
-for node_key, node in nodes.items():
+for node in bubbling_order:
+
     # identify input/output edges to this node, and the corresponding axes
     inp_edges, inp_axes = [], []
     out_edges, out_axes = [], []
@@ -91,18 +91,44 @@ for node_key, node in nodes.items():
             out_edges.append(edge)
             out_axes.append(axis_to_other)
 
+    # identify auxiliary dangling edges that do not participate in swallowing this node
+    aux_edges = [ edge for edge in dangling_edges if edge not in inp_edges ]
+
+    # identify number of input, output, and auxiliary edges (<--> qubits)
+    inp_num = len(inp_edges)
+    out_num = len(out_edges)
+    aux_num = len(aux_edges)
+
+    # total number of qubits necessary to swallow this node
+    total_num = max(inp_num,out_num) + aux_num
+
     # get the tensor associated with this node, reordering axes appropriately
-    tensor = tf.transpose(node.get_tensor(), inp_axes + out_axes)
+    op_tensor = tf.transpose(node.get_tensor(), out_axes + inp_axes)
+
+    # attach ancillary input/output legs to make the swallowing operator "square"
+    op_tensor = tf.tensordot(op_tensor, zero_state(out_num-inp_num), axes = 0) # input ancillas
+    op_tensor = tf.tensordot(zero_state(inp_num-out_num), op_tensor, axes = 0) # output ancillas
+    op_num = len(op_tensor.shape)//2 # qubit tensor factors in the domain/range of op_tensor
+
+    # add ancilla qubits to the state, if necessary
+    state = tf.tensordot(state, zero_state(out_num-inp_num), axes = 0)
 
     # act the swallowing operator on the state
-    state_axes = [ dangling_edges.index(edge) for edge in inp_edges ]
-    tensor_axes = list(range(len(inp_edges)))
-    state = tf.tensordot(state, tensor, axes = [ state_axes, tensor_axes ])
+    op_axes = list(range(op_num,2*op_num)) # second half of the axes
+    inp_node_idx = [ dangling_edges.index(edge) for edge in inp_edges ] # indices of input qubits
+    anc_node_idx = list(range(len(dangling_edges), total_num)) # indices of ancilla qubits
+    state_axes = inp_node_idx + anc_node_idx # indices of qubits addressed by op_tensor
+    state = tf.tensordot(op_tensor, state, axes = [ op_axes, state_axes ])
+
+    # remove (project out) unused qubits from the state
+    state = tf.tensordot(state, zero_state(inp_num-out_num),
+                         axes = [ list(range(inp_num-out_num)),
+                                  list(range(inp_num-out_num)) ] )
 
     # add to our list of "eaten" nodes, update the list of dangling edges
     eaten_nodes.add(node)
     for edge in inp_edges:
         dangling_edges.remove(edge)
-    dangling_edges += out_edges
+    dangling_edges = out_edges + dangling_edges
 
 print(state.numpy())
