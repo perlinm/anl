938c74a252ad7ff16c0dc6edaafb9993f2f140d7
Michael A. Perlin <mika.perlin@gmail.com>
2019-06-21 17:02:52 -0500

    add plots for log(Z)/V, the probability of 'success' and fractional error of the quantum contraction method

--- tensor_networks/evaluate_ising.py
+++ tensor_networks/evaluate_ising.py
@@ -7,14 +7,15 @@ np.set_printoptions(linewidth = 200)
 import tensorflow as tf
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 tf.enable_v2_behavior()
-
 import tensornetwork as tn
 
+import matplotlib.pyplot as plt
 from quantum_contract import quantum_contract
 
-
-inv_temp = 1 # inv_temperature
-lattice_shape = (3,3) # lattice sites per axis
+##########################################################################################
+# method for constructing a tensor network that represents the partition function
+#   of a uniform 2-D Ising model: H = -\sum_{<j,k>} s_j s_k
+##########################################################################################
 
 # value of tensor at a given inv_temperature for particular indices
 def tensor_val(idx, inv_temp):
@@ -22,32 +23,32 @@ def tensor_val(idx, inv_temp):
     return ( 1 + np.prod(s_idx) ) / 2 * np.exp(inv_temp * np.sum(s_idx)/2)
 
 # for a given inv_temperature, return a single tensor in the (translationally-invariant) network
-def make_tensor(inv_temp, shape):
-    tensor_shape = (2,)*len(shape)*2 # dimension of space at each index of the tensor
+def make_tensor(inv_temp, lattice_shape):
+    tensor_shape = (2,)*len(lattice_shape)*2 # dimension of space at each index of the tensor
     tensor_vals = [ tensor_val(idx, inv_temp) for idx in np.ndindex(tensor_shape) ]
     return tf.reshape(tf.constant(tensor_vals), tensor_shape)
 
 # construct tensor network in a hyperrectangular lattice
-def make_net(inv_temp, shape = lattice_shape):
+def make_net(inv_temp, lattice_shape):
     net = tn.TensorNetwork() # initialize empty tensor network
 
     # make all nodes, indexed by lattice coorinates
-    tensor = make_tensor(inv_temp, shape)
+    tensor = make_tensor(inv_temp, lattice_shape)
     nodes = { idx : net.add_node(tensor, name = str(idx))
-              for idx in np.ndindex(shape) }
+              for idx in np.ndindex(lattice_shape) }
 
     # make all edges, indexed by pairs of lattice coordinates
     edges = {}
-    for axis in range(len(shape)): # for each axis of the lattice
+    for axis in range(len(lattice_shape)): # for each axis of the lattice
 
         # choose the axes of tensors that we will contract
-        dir_fst, dir_snd = axis, axis + len(shape)
+        dir_fst, dir_snd = axis, axis + len(lattice_shape)
 
         for idx_fst in nodes: # loop over all nodes
 
             # identify the "next" neighboring node along this lattice axis
             idx_snd = list(idx_fst)
-            idx_snd[dir_fst] = ( idx_snd[dir_fst] + 1 ) % shape[dir_fst]
+            idx_snd[dir_fst] = ( idx_snd[dir_fst] + 1 ) % lattice_shape[dir_fst]
             idx_snd = tuple(idx_snd)
 
             # connect up the nodes
@@ -58,32 +59,54 @@ def make_net(inv_temp, shape = lattice_shape):
 
     return net, nodes, edges
 
-net, nodes, edges = make_net(inv_temp)
-tn.contractors.naive(net)
-net_val = net.get_final_node().tensor.numpy()
-print(net_val)
+##########################################################################################
+# compute various quantities and plot them
+##########################################################################################
 
-net, nodes, edges = make_net(inv_temp)
-net_prob, net_norm = quantum_contract(nodes.values())
-net_val = net_norm * np.sqrt(net_prob)
+steps = 20
+max_inv_temp = 1.5
+lattice_shape = (4,4)
+figsize = (4,3)
 
-print("probability:", net_prob)
-print("network value:", net_val)
+inv_temps = np.linspace(0, max_inv_temp, steps)
+vals_Z = np.zeros(steps)
+probs = np.zeros(steps)
+error = np.zeros(steps)
+for jj in range(steps):
+    net, nodes, edges = make_net(inv_temps[jj], lattice_shape)
+    net_prob, net_norm, qubits_mem, qubits_op = quantum_contract(nodes.values())
+    probs[jj] = net_prob
+    vals_Z[jj] = net_norm * np.sqrt(net_prob)
 
-inv_temps = np.linspace(0, 1.6, 20)
-vals_Z = np.zeros(len(inv_temps))
-for jj, inv_temp in enumerate(inv_temps):
-    net, nodes, edges = make_net(inv_temp)
     tn.contractors.naive(net)
-    net_val = net.get_final_node().tensor.numpy()
-    vals_Z[jj] = net_val
+    tn_val_Z = net.get_final_node().tensor.numpy()
+    error[jj] = vals_Z[jj] / tn_val_Z - 1
 
-import matplotlib.pyplot as plt
+# fractional error of the quantum contraction method
+plt.figure(figsize = figsize)
+plt.plot(inv_temps, error, "k.")
+plt.xlim(0, inv_temps.max())
+plt.ylim(0, plt.gca().get_ylim()[1])
+plt.xlabel(r"$J/T$")
+plt.ylabel(r"$\epsilon$")
+plt.tight_layout()
 
+# log(Z) / V
+plt.figure(figsize = figsize)
 plt.plot(inv_temps, np.log(vals_Z) / np.prod(lattice_shape), "k.")
 plt.xlim(0, inv_temps.max())
 plt.ylim(0, plt.gca().get_ylim()[1])
-plt.xlabel(r"$\beta J$")
+plt.xlabel(r"$J/T$")
 plt.ylabel(r"$\log(Z)/V$")
 plt.tight_layout()
+
+# probability of "success" -- finding all ancillas in |0>
+plt.figure(figsize = figsize)
+plt.plot(inv_temps, probs, "k.")
+plt.xlim(0, inv_temps.max())
+plt.ylim(0, plt.gca().get_ylim()[1])
+plt.xlabel(r"$J/T$")
+plt.ylabel(r"$p$")
+plt.tight_layout()
+
 plt.show()
