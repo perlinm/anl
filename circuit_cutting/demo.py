2e3a2e6b552b6ff8f66482b25948ad34c34e27d5
Michael A. Perlin <mika.perlin@gmail.com>
2019-06-27 10:19:47 -0500

    moved code combining fragment simulation results into fragment_simulator.py cleaned up the demo circuit

--- circuit_cutting/demo.py
+++ circuit_cutting/demo.py
@@ -3,16 +3,13 @@
 import numpy as np
 import qiskit as qs
 
-from itertools import product as set_product
-from functools import reduce
-
 from circuit_cutter import cut_circuit
-from fragment_simulator import get_circuit_distribution, get_fragment_distribution
+from fragment_simulator import get_circuit_distribution, simulate_and_combine
 
 ##########################################################################################
 # construct circuit of random local 2-qubit gates that we can cut
 
-qubits = 5
+qubits = 3
 layers = 2
 
 qreg = qs.QuantumRegister(qubits, "q")
@@ -47,86 +44,21 @@ for old_wire, new_wire in sorted(frag_stitches.items()):
     print(*old_wire, "-->", *new_wire)
 
 ##########################################################################################
+# get distribution functions over measurement outcomes and print results
 
-# convert a distribution vector to a dictionary taking bistrings to values
-# note that the *first* bit in the bitstring corresponds to the *last* bit in a register
+circ_dist = get_circuit_distribution(circ)
+combined_dist = simulate_and_combine(fragments, frag_stitches, frag_wiring, circ.qubits)
+
+# convert a distribution function into dictionary format
 def dist_vec_to_dict(distribution):
     return { "".join([ str(bb) for bb in idx ]) : distribution[idx]
              for idx in np.ndindex(distribution.shape)
              if distribution[idx] != 0 }
 
-# identify initialization and exit wires for each fragment
-def identify_wires(frag_stitches, num_fragments):
-    exit_wires_list, init_wires_list = zip(*frag_stitches.items())
-    exit_wires = tuple([ [ wire[1] for wire in exit_wires_list if wire[0] == frag_idx ]
-                              for frag_idx in range(num_fragments) ])
-    init_wires = tuple([ [ wire[1] for wire in init_wires_list if wire[0] == frag_idx ]
-                              for frag_idx in range(num_fragments) ])
-    return exit_wires, init_wires
-
-# return a dictionary mapping fragment output wires to corresponding wires of a circuit
-def original_wires(original_wires, fragment_wiring, fragment_stitches):
-    wire_map = {}
-    for original_wire in original_wires:
-        frag_wire = fragment_wiring[original_wire]
-        while frag_wire in fragment_stitches:
-            frag_wire = fragment_stitches[frag_wire]
-        wire_map[frag_wire] = original_wire
-    return wire_map
-
-# all allowed assignments of measurement outcomes and states to the ends of a stitch
-stitch_assigments = [ ("+Z",)*2, ("-Z",)*2 ] + [ ( dir_M + op, dir_S + op)
-                                                 for op in [ "X", "Y" ]
-                                                 for dir_M in [ "+", "-" ]
-                                                 for dir_S in [ "+", "-" ] ]
-
-def simulate_and_combine(fragments, frag_wiring, frag_stitches, wire_order):
-    frag_wires = [ fragment.qubits for fragment in fragments ]
-    exit_wires, init_wires = identify_wires(frag_stitches, len(fragments))
-    frag_dists = [ get_fragment_distribution(ff, ii, ee)
-                   for ff, ii, ee in zip(fragments, init_wires, exit_wires) ]
-
-    combined_dist = np.zeros((2,)*len(circ.qubits))
-    for assignment in set_product(stitch_assigments, repeat = len(frag_stitches)):
-        frag_exit_keys = [ set() for _ in range(len(fragments)) ]
-        frag_init_keys = [ set() for _ in range(len(fragments)) ]
-        for stitch_idx, ( exit_frag_wire, init_frag_wire ) in enumerate(frag_stitches.items()):
-            exit_frag_idx, exit_wire = exit_frag_wire
-            init_frag_idx, init_wire = init_frag_wire
-            frag_exit_keys[exit_frag_idx].add(( exit_wire, assignment[stitch_idx][0] ))
-            frag_init_keys[init_frag_idx].add(( init_wire, assignment[stitch_idx][1] ))
-
-        dist_factors = [ frag_dist[frozenset(init_keys), frozenset(exit_keys)]
-                         for frag_dist, init_keys, exit_keys
-                         in zip(frag_dists, frag_init_keys, frag_exit_keys) ]
-
-        scalar_factor = np.product([ ( 1 if fst[0] == snd[0] else -1 ) *
-                                     ( 1 if fst[1] == "Z" else 1/2 )
-                                     for fst, snd in assignment ])
-
-        combined_dist += scalar_factor * reduce(np.multiply.outer, dist_factors[::-1])
-
-    combined_wires = [ ( frag_idx, qubit ) for frag_idx, fragment in enumerate(fragments)
-                       for qubit in fragment.qubits
-                       if ( frag_idx, qubit ) not in frag_stitches.keys() ]
-
-    terminal_wire_map = original_wires(wire_order, frag_wiring, frag_stitches)
-    current_wire_order = [ terminal_wire_map[wire] for wire in combined_wires ]
-    wire_permutation = [ current_wire_order.index(wire) for wire in wire_order ]
-    axis_permutation = [ len(wire_order) - 1 - idx for idx in wire_permutation ][::-1]
-
-    return combined_dist.transpose(*axis_permutation)
-
+# fidelity of two distribution functions: tr( sqrt(rho_0 * rho_1) )
 def distribution_fidelity(dist_0, dist_1):
-    fidelity = 0
-    for idx in np.ndindex(dist_0.shape):
-        fidelity += np.sqrt(abs(dist_0[idx] * dist_1[idx]))
-    return fidelity
-
-
-circ_dist = get_circuit_distribution(circ)
-combined_dist = simulate_and_combine(fragments, frag_wiring, frag_stitches, circ.qubits)
-
+    return sum( np.sqrt(dist_0[idx] * dist_1[idx])
+                for idx in np.ndindex(dist_0.shape) )
 
 print()
 print("full circuit probability distribution")
@@ -140,4 +72,4 @@ for key, val in dist_vec_to_dict(combined_dist).items():
     print(key, val)
 
 print()
-print("inter-distribution fidelity:", distribution_fidelity(circ_dist, combined_dist))
+print("fidelity:", distribution_fidelity(abs(circ_dist), abs(combined_dist)))
--- circuit_cutting/fragment_simulator.py
+++ circuit_cutting/fragment_simulator.py
@@ -3,8 +3,17 @@
 import numpy as np
 import qiskit as qs
 
+from itertools import product as set_product
+from functools import reduce
+
 ##########################################################################################
-# this script simulates circuit fragments
+# this script contains methods to simulate circuit fragments
+# and combine fragment simulation results
+##########################################################################################
+
+##########################################################################################
+# methods to simulate circuit fragments and collect conditional probability distributions
+# over measurement outcomes
 ##########################################################################################
 
 # return an empty circuit on the registers of an existing circuit
@@ -47,27 +56,30 @@ frag_exit_apnds = [ ( "Z", [ ] ),
 # accepts a list of fragments, and lists of wires (i.e. in that fragment) that are
 #   respectively "initialization" wires (on which to prepare states)
 #   and "exit wires" (on which to make measurements)
-# returns an (unnormalized) conditional distribution function in dictionary format:
+# returns an (unnormalized) conditional distribution over measurement outcomes
+#   in dictionary format:
 # { ( set( ( < initialized wire >, < initialized state >   ) ),
 #     set( ( < measured wire >,    < measurement outcome > ) ) :
-#   < non-exit-wire measurement outcomes, conditional on the exit-wire outcomes > }
+#   < distribution over non-exit-wire measurement outcomes,
+#     conditional on the exit-wire measurement outcomes > }
 def get_fragment_distribution(fragment, init_wires = None, exit_wires = None):
     if init_wires: # if we have wires to initialize into various states
 
         # pick the first init wire for state initialization
         init_wire, other_wires = init_wires[0], init_wires[1:]
 
-        frag_dist = {} # distribution function we will return
+        frag_dist = {} # the overall conditional distribution over measurement outcomes
         for state, prepend_op in frag_init_preps: # for each state we will prepare
 
             # construct the circuit to prepare the state we want
             prep_circuit = act_gates(fragment, prepend_op, init_wire)
 
-            # get the distribution function for each prepared state
+            # get the distribution over measurement outcomes for each prepared state
             init_frag_dist = get_fragment_distribution(prep_circuit + fragment,
                                                        other_wires, exit_wires)
 
-            # combine the distribution functions
+            # add to our collection of conditional distributions,
+            # indexing init_frag_dist by state that we prepared
             for all_keys, dist in init_frag_dist.items():
                 init_keys, exit_keys = all_keys
                 new_init_keys = init_keys.union({ ( init_wire, state ) })
@@ -86,26 +98,32 @@ def get_fragment_distribution(fragment, init_wires = None, exit_wires = None):
         # pick the first exit wire for measurement
         exit_wire, other_wires = exit_wires[0], exit_wires[1:]
 
-        # determine axis to delete to get a conditional distribution function
+        # determine axis to delete (i.e. project out) when constructing
+        # a distribution over all measurement outcomes that is conditional
+        # on a particular measurement outcome
         del_axis = -fragment.qubits.index(exit_wire)-1
 
-        frag_dist = {} # distribution function we will return
+        frag_dist = {} # the overall conditional distribution over measurement outcomes
         for measurement, append_op in frag_exit_apnds: # for each measurement basis
 
-            # construct the circuit to measure in the right basis
+            # construct the circuit to measure in the desired basis
             apnd_circuit = act_gates(fragment, append_op, exit_wire)
 
-            # get the conditional distribution function for each measurement
+            # get the conditional distribution for each measurement basis
             exit_frag_dist = get_fragment_distribution(fragment + apnd_circuit,
                                                        init_wires, other_wires)
 
-            # combine the distribution functions
+            # add to our collection of conditional distributions, indexing exit_frag_dist
+            # by the measured basis and measurement outcome on the exit wire
             for all_keys, dist in exit_frag_dist.items():
                 init_keys, exit_keys = all_keys
                 for outcome, bit_state in [ ( "+", 0 ), ( "-", 1 ) ]:
                     new_exit_keys = exit_keys.union({ ( exit_wire, outcome + measurement ) })
+
+                    # project onto a given exit-wire measurement outcome
                     new_dist = np.delete(dist, 1-bit_state, axis = del_axis)
                     new_dist = np.reshape(new_dist, dist.shape[:-1])
+
                     frag_dist[init_keys, new_exit_keys] = new_dist
 
         return frag_dist
@@ -113,3 +131,111 @@ def get_fragment_distribution(fragment, init_wires = None, exit_wires = None):
     # if no init_frag_wires and no exit_frag_wires
     distribution = get_circuit_distribution(fragment)
     return { ( frozenset(), frozenset() ) : distribution }
+
+##########################################################################################
+# methods to combine conditional probability distributions from fragments,
+# and reconstruct the probability distribution of a stitched-together circuit
+##########################################################################################
+
+# identify initialization and exit wires for each fragment
+# accepts dictionary of stitches and total number of fragments
+# returns two lists: one for initialization wires and one exit wires
+# the frag_idx element of each list is itself a list of init/exit wires for that fragment
+def sort_init_exit_wires(frag_stitches, num_fragments):
+    # all exit/init wires: ( frag_index, wire )
+    exit_frag_wires_list, init_frag_wires_list = zip(*frag_stitches.items())
+
+    # collect exit/init wires into list indexed by a fragment number,
+    # removing the fragment index from the wire itself
+    init_wires = tuple([ [ frag_wire[1] for frag_wire in init_frag_wires_list
+                           if frag_wire[0] == frag_idx ]
+                         for frag_idx in range(num_fragments) ])
+    exit_wires = tuple([ [ frag_wire[1] for frag_wire in exit_frag_wires_list
+                           if frag_wire[0] == frag_idx ]
+                         for frag_idx in range(num_fragments) ])
+    return init_wires, exit_wires
+
+# return a dictionary mapping fragment output wires to corresponding wires of a circuit
+def frag_wire_map(circuit_wires, fragment_wiring, fragment_stitches):
+    wire_map = {}
+    for original_wire in circuit_wires:
+        frag_wire = fragment_wiring[original_wire]
+        while frag_wire in fragment_stitches:
+            frag_wire = fragment_stitches[frag_wire]
+        wire_map[frag_wire] = original_wire
+    return wire_map
+
+# rearrange the ordering of wires in a distribution over measurement outcomes
+def rearranged_wires(distribution, old_wire_order, new_wire_order, wire_map = None):
+    if wire_map is None: wire_map = { wire : wire for wire in old_wire_order }
+    current_wire_order = [ wire_map[wire] for wire in old_wire_order ]
+    wire_permutation = [ current_wire_order.index(wire) for wire in new_wire_order ]
+    axis_permutation = [ len(new_wire_order) - 1 - idx for idx in wire_permutation ][::-1]
+    return distribution.transpose(*axis_permutation)
+
+# all allowed assignments of measurement outcomes and states to the ends of a stitch
+stitch_assigments = [ ("+Z",)*2, ("-Z",)*2 ] \
+                  + [ ( dir_exit + op, dir_init + op)
+                      for op in [ "X", "Y" ]
+                      for dir_exit in [ "+", "-" ]
+                      for dir_init in [ "+", "-" ] ]
+
+# simulate fragments and stitch together results
+# accepts a list of circuit fragments and a dictionary for how to stitch them together
+# return a distribution over measurement outcomes on the stitched-together circuit
+def simulate_and_combine(fragments, frag_stitches,
+                         frag_wiring = None, wire_order = None):
+    # collect lists of initialization / exit wires for all fragments
+    init_wires, exit_wires = sort_init_exit_wires(frag_stitches, len(fragments))
+
+    # compute conditional distributions over measurement outcomes for all fragments
+    frag_dists = [ get_fragment_distribution(ff, ii, ee)
+                   for ff, ii, ee in zip(fragments, init_wires, exit_wires) ]
+
+    # identify the order of wires in a combined/reconstructed distribution over outcomes
+    combined_wire_order = [ ( frag_idx, qubit )
+                            for frag_idx, fragment in enumerate(fragments)
+                            for qubit in fragment.qubits
+                            if ( frag_idx, qubit ) not in frag_stitches.keys() ]
+
+    # initialize an empty combined distribution over outcomes
+    combined_dist = np.zeros((2,)*len(combined_wire_order))
+
+    # loop over all assigments of measurement outcomes / initialized states
+    #   at the exit / init wires of all stitches
+    for assignment in set_product(stitch_assigments, repeat = len(frag_stitches)):
+
+        # collect the assignments of exit/init outcomes/states for each fragment
+        frag_exit_keys = [ set() for _ in range(len(fragments)) ]
+        frag_init_keys = [ set() for _ in range(len(fragments)) ]
+        for stitch_idx, ( exit_frag_wire, init_frag_wire ) in enumerate(frag_stitches.items()):
+            exit_frag_idx, exit_wire = exit_frag_wire
+            init_frag_idx, init_wire = init_frag_wire
+            frag_exit_keys[exit_frag_idx].add(( exit_wire, assignment[stitch_idx][0] ))
+            frag_init_keys[init_frag_idx].add(( init_wire, assignment[stitch_idx][1] ))
+
+        # get the conditional probability distribution at each fragment
+        dist_factors = [ frag_dist[frozenset(init_keys), frozenset(exit_keys)]
+                         for frag_dist, init_keys, exit_keys
+                         in zip(frag_dists, frag_init_keys, frag_exit_keys) ]
+
+        # get the scalar factor associated with this assignment of exit/init outcomes/states
+        scalar_factor = np.product([ ( 1 if fst[0] == snd[0] else -1 ) *
+                                     ( 1 if fst[1] == "Z" else 1/2 )
+                                     for fst, snd in assignment ])
+
+        # add to the combined distribution over measurement outcomes
+        combined_dist += scalar_factor * reduce(np.multiply.outer, dist_factors[::-1])
+
+    # if we did not provide wiring info,
+    #   return the combined distribution over measurement outcomes,
+    #   together with the order of wires
+    if frag_wiring is None and wire_order is None:
+        return combined_dist, combined_wire_order
+
+    # otherwise, return the combined distribution over measurement outcomes
+    #   with wires sorted in the provided order
+    else:
+        assert( frag_wiring is not None and wire_order is not None )
+        wire_map = frag_wire_map(wire_order, frag_wiring, frag_stitches)
+        return rearranged_wires(combined_dist, combined_wire_order, wire_order, wire_map)
