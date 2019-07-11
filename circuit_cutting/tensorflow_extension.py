859ea0fdbec19efbe35081e0b4a5eed79392b4df
Michael A. Perlin <mika.perlin@gmail.com>
2019-07-11 15:34:03 -0500

    minor renaming and rearranging methods in tensorflow_extension

--- circuit_cutting/tensorflow_extension.py
+++ circuit_cutting/tensorflow_extension.py
@@ -8,35 +8,35 @@ import tensorflow as tf
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 tf.compat.v1.enable_v2_behavior()
 
-def tf_sparse_add(self, other):
+def _tf_sparse_add(self, other):
     if other == 0: return self
     return tf.sparse.add(self, other)
-tf.SparseTensor.__add__ = tf_sparse_add
+tf.SparseTensor.__add__ = _tf_sparse_add
 
-def tf_sparse_radd(self, other):
+def _tf_sparse_radd(self, other):
     return self + other
-tf.SparseTensor.__radd__ = tf_sparse_radd
+tf.SparseTensor.__radd__ = _tf_sparse_radd
 
-def tf_sparse_mul(self, num):
+def _tf_sparse_neg(self):
+    return -1 * self
+tf.SparseTensor.__neg__ = _tf_sparse_neg
+
+def _tf_sparse_sub(self, other):
+    return self + ( -1 * other )
+tf.SparseTensor.__sub__ = _tf_sparse_sub
+
+def _tf_sparse_mul(self, num):
     if num == 1: return self
     return tf.SparseTensor(self.indices, num * self.values, self.dense_shape)
-tf.SparseTensor.__mul__ = tf_sparse_mul
+tf.SparseTensor.__mul__ = _tf_sparse_mul
 
-def tf_sparse_rmul(self, num):
+def _tf_sparse_rmul(self, num):
     return self * num
-tf.SparseTensor.__rmul__ = tf_sparse_rmul
+tf.SparseTensor.__rmul__ = _tf_sparse_rmul
 
-def tf_sparse_neg(self):
-    return -1 * self
-tf.SparseTensor.__neg__ = tf_sparse_neg
-
-def tf_sparse_sub(self, other):
-    return self + ( -1 * other )
-tf.SparseTensor.__sub__ = tf_sparse_sub
-
-def tf_sparse_truediv(self, other):
+def _tf_sparse_truediv(self, other):
     return 1/other * self
-tf.SparseTensor.__truediv__ = tf_sparse_truediv
+tf.SparseTensor.__truediv__ = _tf_sparse_truediv
 
 def tf_outer_product(tensor_a, tensor_b):
     if type(tensor_a) is not tf.SparseTensor:
