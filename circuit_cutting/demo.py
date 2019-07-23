94f80bfd4ab3a9f7aacb8e11ae1afc0dedb036b4
Michael A. Perlin <mika.perlin@gmail.com>
2019-07-23 16:56:59 -0500

    export some demo methods to their own file

--- circuit_cutting/demo.py
+++ circuit_cutting/demo.py
@@ -12,47 +12,40 @@ from circuit_cutter import cut_circuit
 from fragment_simulator import get_circuit_probabilities, \
     get_fragment_distributions, combine_fragment_distributions
 
+from demo_methods import *
 
-backend_simulator = "statevector_simulator"
-# backend_simulator = "qasm_simulator"
+# backend_simulator = "statevector_simulator"
+backend_simulator = "qasm_simulator"
+
+# number of shots per condition on each fragment
+# irrelevant if using the statevector simulator
 shots = 10**4
-seed = 0
 
-print_circuits = False
-print_distributions = False
+# some printing options
+print_circuits = True
+print_distributions = True
 print_recombination_updates = True
 
 # throw out negative terms when reconstructing a probability distribution?
 discard_negative_terms = False
 
+# convert amplitudes to probabilities?
+# only relevant if using the statevector simulator
+force_probabilities = False
+
+# random number seed
+seed = 0
+
+# numer of qubits and layers in the random unitary circuit
+qubits = 10
+layers = 2
+
 ##########################################################################################
-# construct circuit of random local 2-qubit gates that we can cut
+# build and cut a random unitary circuit
 
-qubits = 20
-layers = 3
+circuit = random_circuit(qubits, layers, seed)
 
-qreg = qs.QuantumRegister(qubits, "q")
-circuit = qs.QuantumCircuit(qreg)
-
-if seed is not None: np.random.seed(seed)
-def random_unitary():
-    return qs.quantum_info.random.utils.random_unitary(4)
-
-for idx, qubit in enumerate(qreg):
-    circuit.u0(idx, qubit)
-circuit.barrier()
-
-for layer in range(layers):
-    for odd_links in range(2):
-        for jj in range(odd_links, qubits-1, 2):
-            random_gate = random_unitary()
-            circuit.append(random_gate, [ qreg[jj], qreg[jj+1] ])
-
-circuit.barrier()
-for idx, qubit in enumerate(qreg):
-    circuit.u0(idx, qubit)
-
-cuts = [ (qreg[qubits//2], op+1) for op in range(1,2*layers) ]
+cuts = [ (circuit.qubits[qubits//2], op+1) for op in range(1,2*layers) ]
 fragments, wire_path_map = cut_circuit(circuit, cuts)
 
 circ_wires = tuple(circuit.qubits)
@@ -62,68 +55,16 @@ if print_circuits:
     print("original circuit:")
     print(circuit)
     print()
-
-    for jj, fragment in enumerate(fragments):
-        print("fragment index:", jj)
-        print(fragment)
-        print("-"*50)
-        print()
-
-    print("wire paths:")
-    print()
-    for wire, path in wire_path_map.items():
-        if len(path) == 1:
-            print(wire, "-->", *path[0])
-        if len(path) > 1:
-           print(wire, "-->")
-           for frag_wire in path:
-               print(" ", *frag_wire)
-    print()
+    print(fragment_info(fragments, wire_path_map))
 
 ##########################################################################################
-# get distribution functions over measurement outcomes and print results
+# simulate circuit, simulate fragments, recombine fragments
 
-# convert a 1-D array of values into a string
-def to_str(vals):
-    return "".join([ str(val) for val in vals ])
-
-# text to print a probability distribution
-def dist_text(dist):
-    if type(dist) is tf.SparseTensor:
-        text_generator = ( f"{to_str(idx.numpy())} {val.numpy()}"
-
-                           for idx, val in zip(dist.indices, dist.values) )
-    else:
-        dist = dist.numpy()
-        text_generator = ( f"{to_str(idx)} {dist[idx]}"
-                           for idx in np.ndindex(dist.shape) )
-    return "\n".join(text_generator)
-
-# fidelity of two distribution functions: tr( sqrt(rho_0 * rho_1) )
-def distribution_fidelity(dist_0, dist_1):
-    if type(dist_0) is tf.SparseTensor and type(dist_1) is not tf.SparseTensor:
-        dist_1 = dist_1.numpy()
-        return sum( np.sqrt(complex(value) * complex(dist_1[tuple(idx)]))
-                    for idx, value in zip(dist_0.indices, dist_0.values) )
-
-    if type(dist_1) is tf.SparseTensor and type(dist_0) is not tf.SparseTensor:
-        return distribution_fidelity(dist_1, dist_0)
-
-    return abs( tf.reduce_sum(tf.sqrt(tf.math.multiply(dist_0, dist_1))).numpy() )**2
-
-# relative entropy S( P | Q ) \equiv tr( P log(P/Q) ) in bits
-# interpretation: information gained upon using Q (the "actual distribution")
-#                 rather than P (the "estimate" of Q)
-def relative_entropy(approx_dist, actual_dist):
-    if tf.SparseTensor in [ type(approx_dist), type(actual_dist) ]: return None
-    dist_to_sum = approx_dist * tf.math.log(approx_dist/actual_dist)
-    return tf.reduce_sum( dist_to_sum ).numpy() / np.log(2)
-
-
-circuit_distribution = get_circuit_probabilities(circuit)
+circuit_distribution = get_circuit_probabilities(circuit, seed_simulator = seed)
 
 frag_distributions \
     = get_fragment_distributions(fragments, wire_path_map, backend_simulator, shots = shots,
+                                 seed_simulator = seed, seed_transpiler = seed,
                                  force_probs = True)
 
 reconstructed_distribution \
@@ -139,5 +80,14 @@ if print_distributions:
     print("reconstructed probability distribution:")
     print(dist_text(reconstructed_distribution))
     print()
+
 print("fidelity:", distribution_fidelity(reconstructed_distribution, circuit_distribution))
 print("relative entropy:", relative_entropy(reconstructed_distribution, circuit_distribution))
+
+# num_vals = np.prod(circuit_distribution.shape)
+# uniform_dist = tf.constant([1/num_vals] * num_vals,
+#                            shape = circuit_distribution.shape,
+#                            dtype = circuit_distribution.dtype)
+# print()
+# print(distribution_fidelity(uniform_dist, circuit_distribution))
+# print(relative_entropy(uniform_dist, circuit_distribution))
---
+++ circuit_cutting/demo_methods.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import qiskit as qs
+
+import os
+import tensorflow as tf
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
+tf.compat.v1.enable_v2_behavior()
+
+# construct circuit of random local 2-qubit gates that we can cut
+def random_circuit(qubits, layers, seed = None):
+
+    qreg = qs.QuantumRegister(qubits, "q")
+    circuit = qs.QuantumCircuit(qreg)
+
+    if seed is not None: np.random.seed(seed)
+    def random_unitary():
+        return qs.quantum_info.random.utils.random_unitary(4)
+
+    for idx, qubit in enumerate(qreg):
+        circuit.u0(idx, qubit)
+    circuit.barrier()
+
+    for layer in range(layers):
+        for odd_links in range(2):
+            for jj in range(odd_links, qubits-1, 2):
+                circuit.append(random_unitary(), [ qreg[jj], qreg[jj+1] ])
+
+    circuit.barrier()
+    for idx, qubit in enumerate(qreg):
+        circuit.u0(idx, qubit)
+
+    return circuit
+
+# human-readable info about a circuit and fragments
+def fragment_info(fragments, wire_path_map):
+    text = ""
+    for jj, fragment in enumerate(fragments):
+        text += f"fragment index: {jj}\n"
+        text += str(fragment.draw()) + "\n"
+        text += "-"*50 + "\n"
+
+    text += "wire paths:\n\n"
+    for wire, path in wire_path_map.items():
+        text += f"{wire} -->"
+        if len(path) == 1:
+            text += " {} {}".format(*path[0])
+        if len(path) > 1:
+           for frag_wire in path:
+               text += "\n {} {}".format(*frag_wire)
+        text += "\n"
+    return text
+
+# convert a 1-D array of values into a string
+def to_str(vals):
+    return "".join([ str(val) for val in vals ])
+
+# text to print a probability distribution
+def dist_text(dist):
+    if type(dist) is tf.SparseTensor:
+        text_generator = ( f"{to_str(idx.numpy())} {val.numpy()}"
+
+                           for idx, val in zip(dist.indices, dist.values) )
+    else:
+        dist = dist.numpy()
+        text_generator = ( f"{to_str(idx)} {dist[idx]}"
+                           for idx in np.ndindex(dist.shape) )
+    return "\n".join(text_generator)
+
+# fidelity of two distribution functions: tr( sqrt(rho_0 * rho_1) )
+def distribution_fidelity(dist_0, dist_1):
+    if type(dist_0) is tf.SparseTensor and type(dist_1) is not tf.SparseTensor:
+        dist_1 = dist_1.numpy()
+        return sum( np.sqrt(complex(value) * complex(dist_1[tuple(idx)]))
+                    for idx, value in zip(dist_0.indices, dist_0.values) )
+
+    if type(dist_1) is tf.SparseTensor and type(dist_0) is not tf.SparseTensor:
+        return distribution_fidelity(dist_1, dist_0)
+
+    return abs( tf.reduce_sum(tf.sqrt(tf.math.multiply(dist_0, dist_1))).numpy() )**2
+
+# relative entropy S( P | Q ) \equiv tr( P log(P/Q) ) in bits
+# interpretation: information gained upon using Q (the "actual distribution")
+#                 rather than P (the "estimate" of Q)
+def relative_entropy(approx_dist, actual_dist):
+    if tf.SparseTensor in [ type(approx_dist), type(actual_dist) ]: return None
+    dist_to_sum = approx_dist * tf.math.log(approx_dist/actual_dist)
+    return tf.reduce_sum( dist_to_sum ).numpy() / np.log(2)
