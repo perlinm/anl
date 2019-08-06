3dc3debe3e530d00c66ef33481ccea928050bf41
Michael A. Perlin <mika.perlin@gmail.com>
2019-08-06 14:53:43 -0500

    fix bug with incorrect data file flags, and add test flag option

--- tensor_networks/clock_formats.py
+++ tensor_networks/clock_formats.py
@@ -5,18 +5,23 @@ import os
 dat_dir = "data"
 fig_dir = "figures"
 
-def dat_name_builder(base_dir, bond_dimension, lattice_size, dimensions, use_vertex, use_XY):
-    text_XY = "_XY" if use_XY else ""
+def _bool_text(use_vertex, use_XY, test):
     text_vertex = "_chkr" if not use_vertex else ""
+    text_XY = "_XY" if use_XY else ""
+    text_test = "_test" if test else ""
+    return test_vertex + text_XY + text_test
+
+def dat_name_builder(base_dir, bond_dimension, lattice_size, dimensions, use_vertex, use_XY, test):
+    bool_text = _bool_text(use_vertex, use_XY, test)
     def _build_method(data_tag):
         base_name = f"{data_tag}_bond{bond_dimension}_N{lattice_size}" \
-                  + f"_D{dimensions}{text_vertex}{text_XY}.txt"
+                  + f"_D{dimensions}{bool_text}.txt"
         return os.path.join(base_dir, base_name)
     return _build_method
 
-def fig_name_builder(base_dir, dimensions, use_XY):
-    text_XY = "_XY" if use_XY else ""
+def fig_name_builder(base_dir, dimensions, use_XY, test):
+    bool_text = _bool_text(use_vertex, use_XY, test)
     def _build_method(data_tag):
-        base_name = f"{data_tag}_D{dimensions}{text_vertex}{text_XY}.pdf"
+        base_name = f"{data_tag}_D{dimensions}{bool_text}.pdf"
         return os.path.join(base_dir, base_name)
     return _build_method
