6cea410a00f43ac19a068fa483f4ee9f038407c8
Michael A. Perlin <mika.perlin@gmail.com>
2019-08-20 22:22:43 -0500

    add flag for test runs in file names

--- tensor_networks/clock_formats.py
+++ tensor_networks/clock_formats.py
@@ -7,14 +7,16 @@ fig_dir = "figures"
 
 def dat_name_builder(base_dir, bond_dimension, lattice_size, dimensions,
                      network_type, test_run):
+    test_text = "_test" if test_run else ""
     def _build_method(data_tag):
         base_name = f"{data_tag}_bond{bond_dimension}_N{lattice_size}" \
-                  + f"_D{dimensions}_{network_type}.txt"
+                  + f"_D{dimensions}_{network_type}{test_text}.txt"
         return os.path.join(base_dir, base_name)
     return _build_method
 
 def fig_name_builder(base_dir, dimensions, network_type, test_run):
+    test_text = "_test" if test_run else ""
     def _build_method(data_tag):
-        base_name = f"{data_tag}_D{dimensions}_{network_type}.pdf"
+        base_name = f"{data_tag}_D{dimensions}_{network_type}{test_text}.pdf"
         return os.path.join(base_dir, base_name)
     return _build_method
